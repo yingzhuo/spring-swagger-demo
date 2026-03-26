080780af2b109d4c9f70848e4dc4ebcb9809fb27
应卓 <yingzhor@gmail.com>
2026-03-26 09:04:00 +0800

    2026-03-26 09:04:00

--- projects-lib/model/src/main/java/io/github/yingzhuo/showcase/model/dto/package-info.java
+++ projects-lib/model/src/main/java/io/github/yingzhuo/showcase/model/dto/package-info.java
@@ -12,7 +12,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */@NullMarked
+ */
+@NullMarked
 package io.github.yingzhuo.showcase.model.dto;
 
 import org.jspecify.annotations.NullMarked;
